f468e5727c3e34f9bf66bd6443fda6dc4717d170
manou141 <manou141@hotmail.com>
2023-04-22 10:56:50 -0400

    first commit

---
+++ app/src/main/java/com/example/homehive/activities/IntroActivity.java
@@ -0,0 +1,24 @@
+package com.example.homehive.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.homehive.R;
+
+public class IntroActivity extends AppCompatActivity {
+    private Button startBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    initView();
+    }
+    private void initView() {
+        startBtn = findViewById(R.id.startBtn);
+        startBtn.setOnClickListener(v-> startActivity(new Intent(IntroActivity.this,MainActivity.class)));
+    }
+}
